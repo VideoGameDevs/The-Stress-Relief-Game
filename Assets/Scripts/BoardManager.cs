72958e3f21e8a821a66d6dd2c630d2d553dba8d6
narosen <1311naro@gmail.com>
2018-03-19 00:41:32 +0530

    initial codes and assets

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+	public static BoardManager board;    
+	public List<Sprite> gemTypes = new List<Sprite>();   
+	public GameObject gem;      
+	public int xSize, ySize;     
+
+	private GameObject[,] tiles;     
+
+	public bool IsShifting { get; set; }    
+
+	void Start () {
+		board = GetComponent<BoardManager>();    
+
+		Vector2 offset = gem.GetComponent<SpriteRenderer>().bounds.size;
+		CreateBoard(offset.x, offset.y);    
+	}
+
+	private void CreateBoard (float xOffset, float yOffset) {
+		tiles = new GameObject[xSize, ySize];    
+
+		float startX = transform.position.x;     
+		float startY = transform.position.y;
+
+		Sprite[] previousLeft = new Sprite[ySize];
+		Sprite previousBelow = null;
+
+		for (int x = 0; x < xSize; x++) {      
+			for (int y = 0; y < ySize; y++) {
+				GameObject newTile = Instantiate(gem, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), gem.transform.rotation);
+				tiles[x, y] = newTile;
+				newTile.transform.parent = transform;
+
+				List<Sprite> possibleCharacters = new List<Sprite>(); 
+				possibleCharacters.AddRange(gemTypes); 
+
+				possibleCharacters.Remove(previousLeft[y]); 
+				possibleCharacters.Remove(previousBelow);
+
+				Sprite newSprite = possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+				newTile.GetComponent<SpriteRenderer>().sprite = newSprite;
+
+				previousLeft[y] = newSprite;
+				previousBelow = newSprite;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour {
+
+	private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
+	private static Gem previousSelected = null;
+
+	private SpriteRenderer render;
+	private bool isSelected = false;
+
+	private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+
+	void Awake() {
+		render = GetComponent<SpriteRenderer>();
+	}
+
+	private void Select() {
+		isSelected = true;
+		render.color = selectedColor;
+		previousSelected = gameObject.GetComponent<Gem>();
+	}
+
+	private void Deselect() {
+		isSelected = false;
+		render.color = Color.white;
+		previousSelected = null;
+	}
+
+	void OnMouseDown() {
+		if (render.sprite == null || BoardManager.board.IsShifting) {
+			return;
+		}
+
+		if (isSelected) { 
+			Deselect();
+		} else {
+			if (previousSelected == null) { 
+				Select();
+			} else {
+				if (GetAllAdjacentTiles().Contains(previousSelected.gameObject)) { 
+					Debug.Log("1");
+					SwapSprite(previousSelected.render); 
+					previousSelected.Deselect();
+				} else { 
+					previousSelected.GetComponent<Gem>().Deselect();
+					Select();
+				}
+			}
+		}
+	}
+
+	public void SwapSprite(SpriteRenderer render2) {
+		if (render.sprite == render2.sprite) { 
+			return;
+		}
+
+		Sprite tempSprite = render2.sprite;
+		render2.sprite = render.sprite; 
+		render.sprite = tempSprite; 
+	}
+
+	private GameObject GetAdjacent(Vector2 castDir) {
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
+		if (hit.collider != null) {
+			Debug.Log (hit.collider.gameObject.GetComponent<SpriteRenderer>().sprite);
+			return hit.collider.gameObject;
+		}
+		return null;
+	}
+
+	private List<GameObject> GetAllAdjacentTiles() {
+		List<GameObject> adjacentTiles = new List<GameObject>();
+		for (int i = 0; i < adjacentDirections.Length; i++) {
+			adjacentTiles.Add(GetAdjacent(adjacentDirections[i]));
+		}
+		return adjacentTiles;
+	}
+}
